7a6d426fd434f9e827f573f02617e3a44c6aaa84
Johannness <145225997+JJdevjoHan@users.noreply.github.com>
2026-04-13 23:10:21 +0800

    Add files via upload
    Used Designer feature in Netbeans and added functionalities and subclasses

---
+++ Calculator/src/calculator/Division.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author JOHAN
+ */
+public class Division implements CalculatorInterface{
+    @Override
+    public double calculate(double a, double b) {
+        if (b == 0) throw new ArithmeticException("Cannot divide by zero");
+        return a / b;
+    }
+}
